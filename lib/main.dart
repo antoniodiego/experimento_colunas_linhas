3a740a4f837ed5b59d9580b289c56777d3059b76
Antonio Diêgo Silva <antoniodiegoluz@hotmail.com>
2022-07-25 00:02:30 -0300

    Primeiros testes

--- lib/main.dart
+++ lib/main.dart
@@ -79,29 +79,47 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Container(
+                margin: const EdgeInsets.symmetric(vertical: 5),
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.black)),
+                child: Row(
+                  children: [
+                    Flexible(
+                        child: Container(
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 1, color: Colors.red)),
+                            child: Column(
+                              children: [
+                                Row(mainAxisSize: MainAxisSize.min, children: [
+                                  Flexible(
+                                      fit: FlexFit.loose,
+                                      child: Container(
+                                          decoration: BoxDecoration(
+                                              border: Border.all(
+                                                  width: 1,
+                                                  color: Colors.blue)),
+                                          child: const Text("Teste"))),
+                                ]),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          width: 1, color: Colors.purple)),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: const [
+                                      Text("Teste 2"),
+                                      Text("Teste 1")
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ))),
+                  ],
+                ))
           ],
         ),
       ),
